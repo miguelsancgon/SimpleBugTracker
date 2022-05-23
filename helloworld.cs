d824c023b37cdf517c3827c1f06747d031f29cb4
Miguel Sánchez <45567363+miguelsancgon@users.noreply.github.com>
2022-05-22 23:27:31 -0500

    helloworld file creation
    helloworld file creation

---
+++ helloworld.cs
@@ -0,0 +1,15 @@
+// C# program to print Hello World!
+using System;
+  
+namespace HelloWorldApp {
+      
+    class SimpleBugTracker {
+         
+        static void Main(string[] args) {
+              
+            Console.WriteLine("Hello World!");
+             
+            Console.ReadKey();
+        }
+    }
+}
